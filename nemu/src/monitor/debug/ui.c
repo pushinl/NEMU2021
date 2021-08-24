b2863ce308af297486af88fe1dca09553a4a62a2
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-24 19:02:06 +0800

    >  compile NEMU 3020234414 pushinl Linux pushinl-virtual-machine 5.8.0-63-generic #71~20.04.1-Ubuntu SMP Thu Jul 15 17:46:08 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:02:06 up 13:43,  1 user,  load average: 0.89, 0.65, 0.58 8fe6609bc0a8f1ab8c1a3a9f1be8aaae465abff9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -67,13 +67,17 @@ static int cmd_x(char *args) {
 	uint32_t ram_addr_start;
 	sscanf(args, " %d %x", &n, &ram_addr_start);
 	//printf("%x", ram_addr_start);
-	int i, j;
+	int i;
 	for(i = 0; i < n; i++) {
 		printf("0x%x: ", ram_addr_start);
+		/*
 		for(j = 0; j < 4; j++){
 			uint32_t data = swaddr_read(ram_addr_start++, 1);
 			printf("0x%02x ", data & 0xff);
-		}
+		}*/
+		uint32_t data = swaddr_read(ram_addr_start, 4);
+		printf("0x%08x ", data);
+		ram_addr_start += 4;
 		printf("\n");
 	}
 	return 0;
