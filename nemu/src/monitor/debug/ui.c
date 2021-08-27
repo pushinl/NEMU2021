9b0f014c053991ad47ae2a50bfd4357a82165525
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-27 14:34:17 +0800

    >  compile NEMU 3020234414 pushinl Linux pushinl-virtual-machine 5.8.0-63-generic #71~20.04.1-Ubuntu SMP Thu Jul 15 17:46:08 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:34:17 up 1 day,  3:32,  1 user,  load average: 0.45, 0.37, 0.26 73dff521d3dc1815cd0b4a7d176e4120a9a84a57

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -63,12 +63,15 @@ static int cmd_info(char *args) {
 
 static int cmd_x(char *args) {
 	if(args == NULL) return 0;
-	int n;
+	int n = 0;
 	bool success = true;
-	char *EXPR = "";
 	uint32_t ram_addr_start;
-	sscanf(args, " %d %s", &n, EXPR);
-	ram_addr_start = expr(EXPR, &success);
+	while(args[0] == ' ') args++;
+	while('0' <= args[0] && args[0] <= '9') n = (n << 3) + (n << 1) + (args[0] & 15), args++;
+	ram_addr_start = expr(args, &success);
+	if(!success) {
+		printf("Expression Error!\n");
+	}
 	//printf("%x", ram_addr_start);
 	int i, cnt = 0;
 	for(i = 0; i < n; i++) {
@@ -84,13 +87,19 @@ static int cmd_x(char *args) {
 		ram_addr_start += 4;
 		if(cnt % 4 == 0) printf("\n");
 	}
+	printf("\n");
 	return 0;
 }
 
 static int cmd_p(char *args){
 	if(args == NULL) return 0;
 	bool success = true;
-	printf("0x%x", expr(args, &success));
+	uint32_t EXPR = expr(args, &success);
+	if(!success) {
+		printf("Expression Error!\n");
+		return 0;
+	}
+	printf("0x%x\n", EXPR);
 	return 0;
 }
 
