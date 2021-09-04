18935d5c647738e60cab44286cf6646bd61222e6
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-04 17:47:23 +0800

    >  compile NEMU 3020234414 pushinl Linux pushinl-virtual-machine 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:47:23 up 11:03,  1 user,  load average: 1.97, 0.82, 0.48 6125e603f0ddf08ef5610e9776aad3c279cf5786

---
+++ nemu/src/cpu/exec/control/call-template.h
@@ -0,0 +1,24 @@
+#include "cpu/exec/template-start.h"
+
+#define instr call
+
+make_helper(concat(call_i_, SUFFIX)) {
+	int len = concat(decode_i_, SUFFIX)(cpu.eip + 1);
+    reg_l(R_ESP) -= DATA_BYTE;
+    swaddr_write(reg_l(R_ESP), 4, cpu.eip + len + 1);
+    DATA_TYPE_S imm = op_src -> val;
+    print_asm("call\t%x",cpu.eip + 1 + len + imm);
+    cpu.eip += imm;
+    return len + 1;
+} 
+
+make_helper(concat(call_rm_, SUFFIX)){
+    int len = concat(decode_rm_, SUFFIX)(cpu.eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	swaddr_write(reg_l(R_ESP) , 4, cpu.eip + len + 1);
+	DATA_TYPE_S imm = op_src -> val;
+	print_asm("call %x",imm);
+	cpu.eip = imm - len - 1;
+	return len + 1;
+}
+#include "cpu/exec/template-end.h"
