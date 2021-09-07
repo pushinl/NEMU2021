93581825aecc33bd15bcc6b54fe94097d99147fa
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-07 16:26:08 +0800

    >  compile NEMU 3020234414 pushinl Linux pushinl-virtual-machine 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:26:08 up 20:01,  1 user,  load average: 0.86, 0.36, 0.29 e1194897a329f03f90a0d753b0ac9eff1c5e4e98

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -42,6 +42,7 @@
 #include "string/scas.h"
 #include "string/stos.h"
 #include "string/movs.h"
+#include "string/lods.h"
 
 #include "misc/misc.h"
 
---
+++ nemu/src/cpu/exec/string/lods.h
@@ -0,0 +1,8 @@
+#ifndef _LODS_H_
+#define _LODS_H_
+
+make_helper(lods_b);
+
+make_helper(lods_v);
+
+#endif
