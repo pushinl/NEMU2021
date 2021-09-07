0c7dd3b650268090f067a32f3db78bf03b6f478c
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-07 16:19:34 +0800

    >  compile NEMU 3020234414 pushinl Linux pushinl-virtual-machine 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:19:34 up 19:54,  1 user,  load average: 0.43, 0.29, 0.27 4be320791b08d2ea434005033753564ad596c71

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -36,6 +36,7 @@
 #include "logic/shr.h"
 #include "logic/shrd.h"
 #include "logic/test.h"
+#include "logic/setcc.h"
 
 #include "string/rep.h"
 #include "string/scas.h"
---
+++ nemu/src/cpu/exec/logic/setcc.h
@@ -0,0 +1,6 @@
+#ifndef _SETCC_H_
+#define _SETCC_H_
+
+make_helper(setne);
+
+#endif
