3553cf76454d7ccd14e2134add1f10179904f767
tracer-NEMU2020 <tjuics2020@163.com>
2021-09-07 19:06:15 +0800

    >  run 3020234414 pushinl Linux pushinl-virtual-machine 5.11.0-27-generic #29~20.04.1-Ubuntu SMP Wed Aug 11 15:58:17 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:06:15 up 22:40,  1 user,  load average: 0.54, 0.45, 0.33 9243265c6605bc846996134082904ce4146ebeb5

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,8 +1,21 @@
 #include "FLOAT.h"
+#include <stdint.h>
+
+typedef union{
+	struct{
+		uint32_t m : 23;
+		uint32_t e : 8;
+		uint32_t s : 1;
+	};
+	uint32_t val;
+}Float;
+
+#define _sign(x) ((x) & 0x80000000)
+#define _scale(x) (_sign(x) ? -(x) : (x))
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	long long ans = 1ll * a * b;
-	return ans >> 16;
+	int64_t scale = ((int64_t)a * (int64_t)b) >> 16;
+	return scale;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
@@ -23,23 +36,9 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 	 * It is OK not to use the template above, but you should figure
 	 * out another way to perform the division.
 	 */
-	
-	
-	int sign = 1;
-	if (a < 0) sign = -sign, a = -a;
-	if (b < 0) sign = -sign, b = -b;
-	int res = a / b;
-	a = a % b;
-	//you can't just return (a/b)<<16
-	//which will lose last 16 bits
-	int i;
-	for (i = 0; i < 16; i++) 
-	{
-		a <<= 1;
-		res <<= 1;
-		if (a >= b) a -= b, ++res;
-	}
-	return res * sign;
+	FLOAT q, r;
+	asm volatile("idiv %2" : "=a"(q), "=d"(r) : "r"(b), "a"(a << 16), "d"(a >> 16));
+	return q;
 }
 
 FLOAT f2F(float a) {
@@ -52,33 +51,26 @@ FLOAT f2F(float a) {
 	 * stack. How do you retrieve it to another variable without
 	 * performing arithmetic operations on it directly?
 	 */
-	int b = *(int *)&a;//read float to int, you can't use (int)a
-	int sign = b & 0x80000000;//sign
-	int exp = (b >> 23) & 0xff;//exp
-	int last = b & 0x7fffff;	//get last 23 bits
-	if(exp == 255) return sign ? -0x7fffffff : 0x7fffffff;//inf, ignore NaN
-	if(exp == 0) return 0;//0, ignore another one
-	last |= 1 << 23;
-	exp -= 134;	//guess why?
-	if (exp < 0) last >>= -exp;
-	if (exp > 0) last <<= exp;
 
-	return sign ? -last : last;
+	Float f;
+	void *temp = &a;
+	f.val = *(uint32_t *)temp;
+	uint32_t m = f.m | (1 << 23);
+	int shift = 134 - (int)f.e;
+	if(shift < 0){
+	m <<= (-shift);
+	}
+	else{
+		m >>= shift;
+	}
+	return (_sign(f.val) ? -m : m);
 }
+
 
 FLOAT Fabs(FLOAT a) {
-	return a < 0 ? -a : a;
+	return _scale(a);
 }
-//Debug Use
-float toFloat(FLOAT s) {
-	int sign = 1, i;
-	if(s < 0)sign = -1,s = -s;
-	float a = 0, as = 0.5;
-	for(i = 15;i >= 0;i--) a += ((s & (1 << i)) >> i) * as, as /= 2;
-	as = 1;
-	for(i = 16;i < 32;i++) a += ((s & (1 << i)) >> i) * as,as *= 2;
-	return sign * a;
-}
+
 /* Functions below are already implemented */
 
 FLOAT sqrt(FLOAT x) {
@@ -104,3 +96,4 @@ FLOAT pow(FLOAT x, FLOAT y) {
 
 	return t;
 }
+
